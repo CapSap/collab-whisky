e8a1468febba8511e06bbb5136771ee206be199e
CapSap <cshotam@gmail.com>
2018-08-14 17:33:56 -0400

    playing around and trying to get a countdown timer to tick/update.

--- src/App.js
+++ src/App.js
@@ -11,45 +11,22 @@ class App extends Component {
         <br />
         <br />
         <br />
+        <WelcomeTest />
         <Children abc = "test" />
         <Child abc = "test" />
-        <SearchBar/>
-        <Button />
+        <Countdown />
       </div>
     );
   }
 }
 
-class SearchBar extends Component {
-  render() {
-    var searchText = "test123";
-    console.log(searchText);
-    return (
-        <form>
-          <input
-            type="text"
-            value={this.props.searchText}
-            // ? this.props.searchText. why?
-            />
-        </form>
-    );
-  }
+
+
+function WelcomeTest() {
+  return <h1> Hello </h1>,
+      <button > Test </button>
 }
 
-class Button extends Component {
-  render() {
-    function buttonClicked() {
-      console.log("testingbutton", {}
-        // value in search bar? how to get that object?
-      )
-    }
-    return(
-      <div>
-        <button type="text" onClick={buttonClicked}> Button </button>
-      </div>
-    );
-  }
-}
 
 class Children extends Component {
   render() {
@@ -69,6 +46,45 @@ const Child  = (props) => {
   );
 };
 
+class Countdown extends Component {
+  render() {
+    const arrive = new Date('September 12, 2018 06:05:00 GMT +10')
+    // from https://gist.github.com/remino/1563878
+    function convertMS(ms) {
+      var d, h, m, s;
+      s = Math.floor(ms / 1000);
+      m = Math.floor(s / 60);
+      s = s % 60;
+      h = Math.floor(m / 60);
+      m = m % 60;
+      d = Math.floor(h / 24);
+      h = h % 24;
+      return (
+        d + " Days " + m + " Minutes " + s + " Seconds "
+      );
+    }
+    console.log(convertMS(2418131631));
+
+    return (
+      <div>
+        <h1> G'day! Landing home in {convertMS(arrive - Date.now())} </h1>
+        </div>
+    )
+  }
+}
+
+// function convertMS(ms) {
+// var d, h, m, s;
+// s = Math.floor(ms / 1000);
+// m = Math.floor(s / 60);
+// s = s % 60;
+// h = Math.floor(m / 60);
+// m = m % 60;
+// d = Math.floor(h / 24);
+// h = h % 24;
+// return { d "Days"
+// }
+// };
 
 
 export default App;
