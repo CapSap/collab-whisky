fc93bf77bdbf5920f209372a507ae591059f9c0d
Kenny Lu <kenny@whynotkenny.com>
2018-08-15 20:40:01 -0400

    remove styles for now to setup js architecture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-router';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
