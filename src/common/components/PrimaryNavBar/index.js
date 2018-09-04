b15f76036c23719be2edfe097a92e04be4dd8278
Kenny Lu <kenny@whynotkenny.com>
2018-09-04 18:15:50 -0400

    add logo & favicon / add file-loader

--- src/common/components/PrimaryNavBar/index.js
+++ src/common/components/PrimaryNavBar/index.js
@@ -1,7 +1,11 @@
+import logo from 'assets/logo.png';
+
 import styles from './styles';
 
 const PrimaryNavBar = () => (
   <div className={styles.base}>
+    <img src={logo} className={styles.logo} alt="IMDB" />
+
     <div>
       menu items
     </div>
