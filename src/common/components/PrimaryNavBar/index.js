76826dd4a0a2cc9d89627f3c4d9da2283fd913d2
Kenny Lu <kenny@whynotkenny.com>
2018-10-24 22:44:56 -0400

    bind state, prepare for submission

--- src/common/components/PrimaryNavBar/index.js
+++ src/common/components/PrimaryNavBar/index.js
@@ -1,5 +1,6 @@
 import logo from 'assets/logo.png';
 import { Select, Button } from 'antd';
+import isEmpty from 'lodash/isEmpty';
 
 import styles from './styles';
 
@@ -21,9 +22,21 @@ class PrimaryNavBar extends React.Component {
     searchQuery: ''
   };
 
-  handleSearch = event => this.setState({ searchQuery: event.target.value });
+  handleInputChange = newState => this.setState(newState);
 
-  handle = event => this.setState({ searchQuery: event.target.value });
+  handleKeyPress = event => {
+    const { searchQuery } = this.state;
+
+    if (event.key === 'Enter' || (event.keyCode === 13 && !isEmpty(searchQuery))) {
+      this.submit();
+    }
+  };
+
+  submit = () => {
+    const { searchQuery, selectedCategory } = this.state;
+
+    console.log(`query TMDB, searching for "${searchQuery}" under "${selectedCategory}"`);
+  };
 
   render() {
     const { selectedCategory, searchQuery } = this.state;
@@ -36,10 +49,15 @@ class PrimaryNavBar extends React.Component {
           <input
             placeholder="Find Movies, TV Shows, Celebrities and more..."
             value={searchQuery}
-            onChange={this.handleSearch}
+            onChange={event => this.handleInputChange({ searchQuery: event.target.value })}
+            onKeyPress={this.handleKeyPress}
           />
 
-          <Select defaultValue={selectedCategory} style={{ height: '100%', width: '150px' }}>
+          <Select
+            defaultValue={selectedCategory}
+            style={{ height: '100%', width: '150px' }}
+            onChange={value => this.handleDropdownSelect({ selectedCategory: value })}
+          >
             {menuCategories.map((menuItem, index) => (
               <Select.Option value={menuItem.value} key={`${menuItem.value}-${index}`}>
                 {menuItem.name}
@@ -50,6 +68,7 @@ class PrimaryNavBar extends React.Component {
           <Button
             style={{ backgroundColor: '#F5C518', fontWeight: 'bold', color: '#000' }}
             icon="search"
+            onClick={this.submit}
           />
         </div>
 
