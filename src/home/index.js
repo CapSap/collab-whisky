3451025b5f0f89630265a8b320b6f16b2c599cfe
CapSap <cshotam@gmail.com>
2018-10-26 22:50:45 +1100

    delete coundown

--- src/home/index.js
+++ src/home/index.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import SearchBar from './searchbar';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -8,31 +9,12 @@ class HomePage extends React.Component {
       count: '',
     };
 
-    setInterval(this.tidyTime, 20);
   }
 
-  // from https://gist.github.com/remino/1563878
-  convertMS = (ms) => {
-    var d, h, m, s;
-    s = Math.floor(ms / 1000);
-    m = Math.floor(s / 60);
-    s = s % 60;
-    h = Math.floor(m / 60);
-    m = m % 60;
-    d = Math.floor(h / 24);
-    h = h % 24;
-    ms = ms % 1000;
 
-    return (
-      d + " Days, " + m + " Minutes, " + s + " Seconds, " + ms + " Fast seconds"
-    );
-  }
+ 
 
-  tidyTime = () => {
-    const arrive = new Date('September 12, 2018 06:05:00 GMT +10');
-
-    this.setState({ count: this.convertMS(arrive - Date.now()) });
-  }
+  
 
   render() {
     const { isLoading } = this.props;
@@ -44,7 +26,7 @@ class HomePage extends React.Component {
     return (
       <div>
         <h1>G'day! Landing home in {this.state.count}</h1>
-        <h2>test</h2>
+        <SearchBar />
       </div>
     );
   }
