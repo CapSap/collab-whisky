6b15fc142ebb4c7b797778d391149fc09d513096
Kenny Lu <kenny@whynotkenny.com>
2018-10-28 15:02:39 -0400

    rename image url in conf

--- src/conf.js
+++ src/conf.js
@@ -2,5 +2,5 @@ export default {
   API_URL: 'https://api.themoviedb.org/3',
   API_KEY: 'a9245ee229ec8157ca8c14b253fc31c7',
   defaultQueryParams: '&language=en-US&page=1&include_adult=false',
-  IMAGE_API: 'https://image.tmdb.org/t/p'
+  IMAGE_URL: 'https://image.tmdb.org/t/p'
 };
