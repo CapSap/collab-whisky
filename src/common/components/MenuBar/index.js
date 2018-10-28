afac80d60989dd819afb6b9549a26280ce21dec8
Kenny Lu <kenny@whynotkenny.com>
2018-10-28 12:11:23 -0400

    update menu submit method to dispatch async request / catch loading & display button as loading

--- src/common/components/MenuBar/index.js
+++ src/common/components/MenuBar/index.js
@@ -1,7 +1,10 @@
 import logo from 'assets/logo.png';
-import { Input, Select, Button } from 'antd';
+import { Input, Select, Button, Icon } from 'antd';
 import isEmpty from 'lodash/isEmpty';
+import Spinner from 'components/Spinner';
+import { connect } from 'react-redux';
 
+import { fetchMoviesViaQuery } from './actions';
 import styles from './styles';
 
 const menuCategories = [
@@ -28,6 +31,11 @@ const menuCategories = [
 ];
 
 class MenuBar extends React.Component {
+  static propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    fetchMovies: PropTypes.func.isRequired
+  };
+
   state = {
     selectedCategory: menuCategories[0].value,
     searchQuery: ''
@@ -43,14 +51,19 @@ class MenuBar extends React.Component {
 
   submit = () => {
     const { searchQuery, selectedCategory } = this.state;
+    const { isLoading, fetchMovies } = this.props;
 
-    console.log(`query TMDB, searching for "${searchQuery}" under "${selectedCategory}"`);
-    if (isEmpty(searchQuery)) {
+    if (isEmpty(searchQuery) || isLoading) {
+      return;
     }
+
+    fetchMovies(searchQuery, selectedCategory);
   };
 
   render() {
     const { selectedCategory, searchQuery } = this.state;
+    const { isLoading } = this.props;
+    console.log('this.props: ', this.props);
 
     return (
       <div className={styles.base}>
@@ -67,7 +80,7 @@ class MenuBar extends React.Component {
           <Select
             defaultValue={selectedCategory}
             style={{ height: '100%', width: '150px' }}
-            onChange={value => this.handleDropdownSelect({ selectedCategory: value })}
+            onChange={value => this.handleInputChange({ selectedCategory: value })}
           >
             {menuCategories.map((menuItem, index) => (
               <Select.Option value={menuItem.value} key={`${menuItem.value}-${index}`}>
@@ -83,9 +96,11 @@ class MenuBar extends React.Component {
               color: '#000',
               border: 'none'
             }}
-            icon="search"
+            disabled={isLoading}
             onClick={this.submit}
-          />
+          >
+            {isLoading ? <Spinner /> : <Icon type="search" />}
+          </Button>
         </div>
 
         <div>menu items</div>
@@ -94,4 +109,11 @@ class MenuBar extends React.Component {
   }
 }
 
-export default MenuBar;
+const mapState = ({ menu }) => ({ ...menu });
+
+const mapDispatch = { fetchMovies: fetchMoviesViaQuery };
+
+export default connect(
+  mapState,
+  mapDispatch
+)(MenuBar);
