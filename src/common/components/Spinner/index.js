fcf6309cb892892b9bd4dafb73520ba4e23773f0
Kenny Lu <kenny@whynotkenny.com>
2018-10-28 12:09:28 -0400

    Setup loading Spinner

---
+++ src/common/components/Spinner/index.js
@@ -0,0 +1,7 @@
+import { Spin, Icon } from 'antd';
+
+export default ({ style, restProps }) => (
+  <Spin
+    indicator={<Icon type="loading" {...restProps} style={style || { fontSize: '2em' }} spin />}
+  />
+);
