88039ac7cd4f60f093ade820b6d3274a602c5eb2
Kenny Lu <kenny@whynotkenny.com>
2018-10-28 13:27:05 -0400

    use lifecycle in ResultsPage to go home when nothing to display

--- src/results-page/index.js
+++ src/results-page/index.js
@@ -1,38 +1,60 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import Page from 'components/Page';
 import NotFound from 'components/NotFound';
 import isEmpty from 'lodash/isEmpty';
 
-const ResultsPage = ({ isLoading, hasError, payload }) => {
-  if (isLoading) {
-    return <div>loading</div>;
+class ResultsPage extends React.Component {
+  static propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    hasError: PropTypes.shape({}),
+    payload: PropTypes.shape({}),
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }).isRequired
+  };
+
+  static defaultProps = {
+    hasError: null,
+    payload: null
+  };
+
+  componentDidMount() {
+    const { isLoading, payload, history } = this.props;
+
+    if (!isLoading && isEmpty(payload)) {
+      history.push('/');
+    }
   }
 
-  if (hasError || (payload && isEmpty(payload.data))) {
-    return <NotFound error={hasError} />;
+  render() {
+    const { isLoading, hasError, payload } = this.props;
+
+    if (isLoading) {
+      return <div>loading</div>;
+    }
+
+    if (hasError || (payload && isEmpty(payload.data))) {
+      return <NotFound error={hasError} />;
+    }
+
+    if (isEmpty(payload)) {
+      return null;
+    }
+
+    console.log('payload: ', payload);
+    const data = payload.data.results;
+    console.log('data: ', data);
+
+    return (
+      <Page.Content center>
+        <div>Hello</div>
+        <span>world</span>
+      </Page.Content>
+    );
   }
-
-  console.log('payload: ', payload);
-  const data = payload.data.results;
-  console.log('data: ', data);
-
-  return (
-    <Page.Content center>
-      <div>Hello</div>
-      <span>world</span>
-    </Page.Content>
-  );
-};
-ResultsPage.propTypes = {
-  isLoading: PropTypes.bool.isRequired,
-  hasError: PropTypes.shape({}),
-  payload: PropTypes.shape({})
-};
-ResultsPage.defaultProps = {
-  hasError: null,
-  payload: null
-};
+}
 
 const mapState = ({ menu }) => ({ ...menu });
 
-export default connect(mapState)(ResultsPage);
+export default withRouter(connect(mapState)(ResultsPage));
